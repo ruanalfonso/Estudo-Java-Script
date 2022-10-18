713d17bc9db45500e1315b3cdb02fd5322211dbb
ruanalfonso <ruan.alfonso@yahoo.com.br>
2022-10-18 07:50:40 -0300

    commit

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 10
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log (`Diriga sempre usando o cinto de segurança`)
