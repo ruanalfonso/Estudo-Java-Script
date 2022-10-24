2a5f9beb3771973f3c545b4ef70e3ad824973c06
ruanalfonso <ruan.alfonso@yahoo.com.br>
2022-10-24 07:53:11 -0300

    1

--- projetos/Projeto 1/script.js
+++ projetos/Projeto 1/script.js
@@ -2,11 +2,20 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date ()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 22
 msg.innerHTML = ` Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12 ) {
     //bom dia!
-    img.src = 'fotomanha.png'
-
+    img.src = 'imagens/fotomanhã.png'
+    document.body.style.background = '#e2cd9f'
+} else if ( hora >= 12 && hora <= 18 ) {
+  //Boa tarde!
+  img.src ='imagens/fototarde.png'
+  document.body.style.background = '#b9846f'
+} else {
+    img.src = 'imagens/fotonoite.png'
+    document.body.style.background = '#515154'
 }
+
 }
