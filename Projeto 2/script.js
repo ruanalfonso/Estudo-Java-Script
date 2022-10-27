788972b9d01cb33cd50aa06bd2be9052bf82b25f
ruanalfonso <ruan.alfonso@yahoo.com.br>
2022-10-27 16:23:35 -0300

    .

--- Projeto 2/script.js
+++ Projeto 2/script.js
@@ -6,20 +6,34 @@ function verificar(){
   if (fano.value.length ==0 || Number(fano.value) > ano) {
       window.alert('[ERRO] Verifique os dados e tente novamente')
   } else {
-        var fsex = document.getElementsByTagName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
-            if (idade >= &&  idade < 10) {
-              
+            if (idade >=0 && idade < 10){
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50 ) {
+                //Adulto
+            } else {
+                //idoso
             }
-        } else if (fsex[1].checked) {
+        } else if (fsex[1].checked) 
             gênero = 'Mulher'
-        }
+            if (idade >=0 && idade < 10){
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50 ) {
+                //Adulto
+            } else {
+                //idoso
+            }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.insertAdjacentHTML = ` Detecatamos ${gênero} com idade ${idade} anos. `
   }
 }
---
+++ aula 07/ambiente.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c <=5) {
+    console.log(c)
+    c++
+}
+*/
+
+console.log('Vai Começar ....')
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+console.log('FIM!')
